4aa18c3b88757c6fa49af732711000205d12a055
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-03-20 22:14:36 +0100

    20.03.2023 at 22:14

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    
+}
