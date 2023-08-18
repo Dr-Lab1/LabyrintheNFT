49cdf26be048ee68086e3a2a774e4c3fe25b600b
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-08-18 18:53:34 +0200

    Hero - 18.08.2023 at 18:53

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,28 @@
 import img from '../assets/1434_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDYvNGE4NmNmOWQtODM2Mi00YmVhLThiMzctZDEyODAxNjUxZTE1LmpwZWc=.jpg'
 import Identicon from 'react-identicons'
-import { setGlobalState } from '../store'
+import { setGlobalState, truncate, useGlobalState } from '../store'
 
 const Hero = () => {
+
+    const [connectedAccount] = useGlobalState('connectedAccount')
+    const onCreatedNFT = () => {
+        setGlobalState('modal', 'scale-100')
+    }
+
     return (
         <div className="flex flex-col justify-between items-center md:flex-row w-4/5 mx-auto py-10">
             <div className="md:w-3/6 w-full">
                 <div>
                     <h1 className="text-white text-5xl font-bold">
                         Vendre et acheter <br /> l'Art Digital, <br />
-                        La Collection <span className="text-gradient">NFTs</span> 
+                        La Collection <span className="text-gradient">NFTs</span>
                     </h1>
                     <p className="text-gray-500 font-semibold text-sm mt-3">Minter et collectionner les NFTs</p>
                 </div>
 
                 <div className="mt-2">
                     <button className="shadow-xl shadow-black text-white p-2 rounded-full bg-[#e32970] hover:bg-[#bd255f] md:text-xs"
-                    onClick={() => {setGlobalState('modal', 'scale-100')}}>
+                        onClick={() => { setGlobalState('modal', 'scale-100') }}>
                         Créer un NFT
                     </button>
                 </div>
@@ -43,9 +49,16 @@ const Hero = () => {
                 <img className='h-60 w-full object-cover' src={img} alt="img" />
 
                 <div className='flex justify-start items-center p-3'>
-                    <Identicon className="h-10 w-10 object-contain rounded-full mr-3" string = {"0x21...786a"} size = {50}/>
+                    <Identicon className="h-10 w-10 object-contain rounded-full mr-3" string={"0x21...786a"} size={50} />
                     <div>
-                        <p className='text-white font-semibold'>0x21...786a</p>
+                        <p className='text-white font-semibold'>
+                            {connectedAccount ? 
+                            (
+                                truncate(connectedAccount, 4, 4, 11)
+                            ) : (
+                                null
+                            ) }
+                        </p>
                         <small className='text-pink-800 font-bold'>@you</small>
                     </div>
                 </div>
