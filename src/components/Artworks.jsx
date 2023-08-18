314df8d15ffef317e3a23049922654a200b4bd03
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-08-18 03:32:46 +0200

    ArtWorks : setNFT - 18.08.2023 at 03:32

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -3,7 +3,7 @@ import { setGlobalState, useGlobalState } from '../store'
 const Artworks = () => {
 
     const [nfts] = useGlobalState('nfts')
-    
+
     return (
         <div className="bg-[#151c25] gradient-bg-artworks">
             <div className="w-4/5 py-10 mx-auto">
@@ -25,24 +25,32 @@ const Artworks = () => {
     )
 }
 
-const Card = ({nft}) => (
-    <div className='w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3'>
-        <img className='h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3' src={nft.metadataURI} alt={nft.title} />
-        <h4 className='text-white font-semibold'>{nft.title}</h4>
-        <p className='w-full text-gray-400 text-sm my-1'>{nft.description}</p>
+const Card = ({ nft }) => {
 
-        <div className='flex justify-between items-center mt-3 text-white'>
-            <div className='flex flex-col'>
-                <small className='text-xs'>Prix actuel</small>
-                <p className='text-sm font-semibold'>{nft.price} ETH</p>
-            </div>
-            <button className='shadow-lg shadow-black text-sm bg-[#e32970] hover:bg-[#bd255f] 
+    const setNFT = () => {
+        setGlobalState('nft', nft)
+        setGlobalState('showModal', 'scale-100')
+    }
+    
+    return (
+        <div className='w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3'>
+            <img className='h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3' src={nft.metadataURI} alt={nft.title} />
+            <h4 className='text-white font-semibold'>{nft.title}</h4>
+            <p className='w-full text-gray-400 text-sm my-1'>{nft.description}</p>
+
+            <div className='flex justify-between items-center mt-3 text-white'>
+                <div className='flex flex-col'>
+                    <small className='text-xs'>Prix actuel</small>
+                    <p className='text-sm font-semibold'>{nft.cost} ETH</p>
+                </div>
+                <button className='shadow-lg shadow-black text-sm bg-[#e32970] hover:bg-[#bd255f] 
                     rounded-full px-1.5 py-1'
                     onClick={() => setGlobalState('showModal', 'scale-100')}>
-                Plus de détails
-            </button>
+                    Plus de détails
+                </button>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Artworks
