52d189f89e8f2cc005d89e9e720d476699afba92
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-08-18 03:35:19 +0200

    ArtWorks : setNFT - 18.08.2023 at 03:35

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -31,7 +31,7 @@ const Card = ({ nft }) => {
         setGlobalState('nft', nft)
         setGlobalState('showModal', 'scale-100')
     }
-    
+
     return (
         <div className='w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3'>
             <img className='h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3' src={nft.metadataURI} alt={nft.title} />
@@ -45,7 +45,8 @@ const Card = ({ nft }) => {
                 </div>
                 <button className='shadow-lg shadow-black text-sm bg-[#e32970] hover:bg-[#bd255f] 
                     rounded-full px-1.5 py-1'
-                    onClick={() => setGlobalState('showModal', 'scale-100')}>
+                    onClick={setNFT}
+                    >
                     Plus de détails
                 </button>
             </div>
