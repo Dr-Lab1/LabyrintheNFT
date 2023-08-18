c65200aec9766a674a38429e223ad307d6e52e9c
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-08-18 22:30:14 +0200

    truffle-config.js - 18.08.2023 at 22:29

--- truffle-config.js
+++ truffle-config.js
@@ -11,6 +11,14 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id
     },
+    goerli : {
+      provider: () => new HDWalletProvider(process.env.SECRET_KEY, process.env.ENDPOINT_URL),
+      network_id: 5,
+      gas : 5500000,
+      confirmations : 2,
+      timeoutBlocks : 200,
+      skipDryRun : true
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
